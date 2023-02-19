6cacb32633447675f0611345a9a000b0310b1541
PerseusPotter <danktha0s@gmail.com>
2023-02-18 21:44:05 -0500

    add createElems to util

--- src/util.js
+++ src/util.js
@@ -33,3 +33,56 @@ var attemptLogin;
     } else throw "Invalid username/password combination";
   };
 })();
+
+var createElems = (function() {
+  /*
+  tags
+  [
+    'span',
+    {
+      tag: 'div',
+      class: 'header'
+    },
+    [
+      {
+        tag: 'p',
+        class: [ 'text', 'center' ]
+      }
+    ]
+  ]
+  <span></span>
+  <div class="header">
+    <p class="text center"></p>
+  <div>
+  */
+  let prepareTags = function(tags) {
+    return tags.map(v => {
+      if (Array.isArray(v)) return prepareTags(v);
+      if (typeof v === 'string' || v instanceof String) return { tag: v };
+      return v;
+    });
+  };
+  let create = function(tag, parent) {
+    let elem = parent;
+    if (Array.isArray(tag)) {
+      tag.forEach(v => {
+        elem = create(v, Array.isArray(v) ? elem : parent) || parent;
+      });
+      return (parent ? null : elem);
+    }
+    elem = document.createElement(tag.tag);
+    if (tag.class) (Array.isArray(tag.class) ? tag.class : [tag.class]).forEach(v => elem.classList.add(v));
+    if (tag.id) elem.id = tag.id;
+    if (parent) parent.append(elem);
+    return elem;
+  };
+  return function(tags) {
+    if (!Array.isArray(tags)) tags = [tags];
+    if (Array.isArray(tags[0])) tags.unshift('div');
+    let parent = tags.shift();
+    parent = create(parent);
+    tags = prepareTags(tags, parent);
+    create(tags, parent);
+    return parent;
+  };
+}());
