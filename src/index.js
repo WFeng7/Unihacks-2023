906dd3a60df29b8c7bb939e8359e405313e959ba
ugackMiner <ugackMiner@gmail.com>
2023-02-18 20:11:42 -0500

    Ziyang it goes in the scss file not css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const signedIn = (localStorage.getItem("token") ? true : false);
+let accountButton = document.getElementById("accountButton");
+
+if (signedIn) {
+    accountButton.innerText = "Logout";
+    document.getElementById("signupButton").style.display = "none";
+} else {
+    document.getElementById("dashboardLink").style.display = "none";
+}
+
+accountButton.onclick = () => {
+    if (signedIn) {
+        localStorage.removeItem("token");
+        location.reload();
+    } else {
+        location.href = "/login";
+    }
+}
