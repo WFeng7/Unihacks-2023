f25e37b7a1d647b012e9729fcf140b7d5b52c1c1
ugackMiner <ugackMiner@gmail.com>
2023-02-19 13:17:14 -0500

    why did this not push before what

--- src/dashboard.js
+++ src/dashboard.js
@@ -6,3 +6,30 @@ document.getElementById("accountButton").onclick = () => {
     localStorage.removeItem("token");
     location.href = "./index.html";
 }
+
+function createPatientElement(uuid, name) {
+    let dashboard = document.getElementById("dashboard");
+    // Yes, updating innerHTML is slow. It's better than 18 lines of setting everything individually
+    dashboard.innerHTML += `<div class="patient" data-bs-toggle="modal" data-bs-target="#viewPatientModal" data-uuid="${uuid}" onclick="patientClick(this)"><p class="name">${name}</p></div>`;
+
+}
+
+function patientClick(element) {
+    console.log(`UUID: ${element.dataset.uuid}, Name: ${element.children[0].innerText}`);
+}
+
+
+function getPatients() {
+    // Get the patients from the API
+
+}
+
+
+function createPatient() {
+
+}
+
+// Get the patients here instead of setting them manually
+createPatientElement("34298089vce9rwij234b,dsf", "big mama");
+createPatientElement("34298089vce9rwij234b,dsf", "joe chungu");
+createPatientElement("34298089vce9rwij234b,dsf", "apple");
